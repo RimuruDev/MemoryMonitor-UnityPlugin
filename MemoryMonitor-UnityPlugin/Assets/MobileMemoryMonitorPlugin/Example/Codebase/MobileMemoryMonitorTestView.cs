adc8f261cd818db8fa9a50678982249d610c29f1
Rimuru <Rimuru.Dev@gmail.com>
2025-01-13 18:35:40 +0400

    Created JavaBridge

---
+++ MemoryMonitor-UnityPlugin/Assets/MobileMemoryMonitorPlugin/Example/Codebase/MobileMemoryMonitorTestView.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using UnityEngine;
+
+namespace AbyssMoth.MobileMemoryMonitorPlugin.Example.Codebase
+{
+    public class MobileMemoryMonitorTestView : MonoBehaviour
+    {
+        public TMP_Text DebugText;
+
+        private void Awake()
+        {
+            
+        }
+
+        private void Update()
+        {
+            
+        }
+    }
+}
---
+++ MemoryMonitor-UnityPlugin/Assets/MobileMemoryMonitorPlugin/Runtime/Android/MMMJavaBridge.cs
@@ -0,0 +1,65 @@
+// ReSharper disable MemberCanBePrivate.Global
+
+using UnityEngine;
+
+namespace AbyssMoth.MobileMemoryMonitorPlugin.Runtime.Android
+{
+    internal class MMMJavaBridge
+    {
+        internal const string UNITY_PLAYER = "com.unity3d.player.UnityPlayer";
+        internal const string CURRENT_ACTIVITY = "currentActivity";
+
+        internal const string PLUGIN_PACKAGE = "com.abyssmoth.mobilememorymonitor";
+        internal const string RAM_MONITOR = PLUGIN_PACKAGE + ".RAMMonitor";
+        internal const string SDK_MONITOR = PLUGIN_PACKAGE + ".SDKMonitor";
+        internal const string STORAGE_MONITOR = PLUGIN_PACKAGE + ".StorageMonitor";
+
+        public static AndroidJavaObject GetContext()
+        {
+            using var unityPlayer = new AndroidJavaClass(UNITY_PLAYER);
+
+            return unityPlayer.GetStatic<AndroidJavaObject>(CURRENT_ACTIVITY);
+        }
+
+        public static void Call(string methodName, params object[] args)
+        {
+            using var plugin = new AndroidJavaClass(PLUGIN_PACKAGE);
+
+            plugin.CallStatic(methodName, args);
+        }
+
+        public static TResult CallStatic<TResult>(string methodName, params object[] args)
+        {
+            using var plugin = new AndroidJavaClass(PLUGIN_PACKAGE);
+
+            return plugin.CallStatic<TResult>(methodName, args);
+        }
+    }
+
+    internal class RAMMonitorClient
+    {
+    }
+
+    internal class SDKMonitorClient
+    {
+        // Methods ===
+        private const string ReadSDKVersion = "ReadSDKVersion";
+        private const string HandleFeatureSupport = "HandleFeatureSupport";
+
+        public string GetSDKVersion()
+        {
+            return MMMJavaBridge.CallStatic<string>(ReadSDKVersion);
+        }
+
+        public int CheckFeatureSupport(int requiredApiLevel)
+        {
+            // ERROR_LOW_API = -1
+            // FEATURE_SUPPORTED = 0
+            return MMMJavaBridge.CallStatic<int>(HandleFeatureSupport, requiredApiLevel);
+        }
+    }
+
+    internal class StorageMonitorClient
+    {
+    }
+}
